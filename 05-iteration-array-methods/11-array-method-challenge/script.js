d4ba23cd21d6fe659a48b8c197ebce5366c09e8b
sofian-eth <itxsofianshafqat@gmail.com>
2024-10-14 17:47:54 +0500

    new challenge completed

--- 05-iteration-array-methods/11-array-method-challenge/script.js
+++ 05-iteration-array-methods/11-array-method-challenge/script.js
@@ -61,3 +61,27 @@ const words = ['coder', 'programmer', 'developer']
 const capitalizedWords = words.map(word => word[0].toUpperCase() + word.slice(1));
 
 console.log(capitalizedWords);
+
+//////////////////////////////////////////////////////////////////
+
+const students = [
+    { name: 'Alice', age: 20, score: 85 },
+    { name: 'Bob', age: 22, score: 78 },
+    { name: 'Charlie', age: 23, score: 95 },
+    { name: 'David', age: 21, score: 66 },
+    { name: 'Eve', age: 22, score: 88 }
+];
+
+const useFilter = students.filter(student => student.score >= 80);
+console.log(useFilter);
+
+const useMap = useFilter.map(student => student.name);
+console.log(useMap);
+
+const useReduce = students.reduce((acc, curr) => (acc + curr.score), 0);
+const useReduceAnswer = useReduce/students.length;
+console.log(useReduceAnswer);
+
+students.forEach(student => {
+    console.log(student.score >= 80? `${student.name} - passed!`:`${student.name} - failed!`)
+})
